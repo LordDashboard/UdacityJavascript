aa6b47408b4c8f286e0303b1ca1e199e388b67f1
Ward Venrooij <ward@data-ink.nl>
2023-05-16 22:07:28 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 
     // Create Dino Constructor
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+    }
 
 
     // Create Dino Objects
+    const triceratops = new Dino(
+        "Triceratops",
+        13000,
+        114,
+        "herbavor",
+        "North America",
+        "Late Cretaceous",
+        "First discovered in 1889 by Othniel Charles Marsh"
+    );
 
 
     // Create Human Object
+    const human = {
+        name: "John",
+        height: 180,
+        weight: 80,
+        diet: "omnivore"
+    };
 
     // Use IIFE to get human data from form
+    (function() {
+        const form = document.getElementById("dino-compare");
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const name = document.getElementById("name").value;
+            const feet = document.getElementById("feet").value;
+            const inches = document.getElementById("inches").value;
+            const weight = document.getElementById("weight").value;
+
+        });
+    })();
+
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches
+     
 
     
     // Create Dino Compare Method 2
